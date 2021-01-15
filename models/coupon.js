ce6585969144ce25aa6a1b0e4a6fe3eb502377b2
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-01-15 12:08:06 -0600

    bug fixes

--- models/coupon.js
+++ models/coupon.js
@@ -9,8 +9,8 @@ const couponSchema = new mongoose.Schema(
             unique: true,
             uppercase: true,
             required: 'Name is required',
-            minlength: [4, "Too short"],
-            maxlength: [20, 'Too long']
+            // minlength: [4, "Too short"],
+            // maxlength: [20, 'Too long']
         },
         expiry: {
             type: Date,

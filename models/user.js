62c2767f33d17aed242220b9e87436fc9770d661
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-01-16 10:07:56 -0600

    update user modal

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
         },
         address: {
             type: String,
-            required: true,
+            // required: true,
         },
         wishlist: [{ type: ObjectId, ref: "Product" }],
     },
